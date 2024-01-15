0ae7ba5e608f8933225af16b9b14800c71963d6f
scroogemcfawk <scroogemcfawk@gmail.com>
2024-01-15 21:30:55 +0700

    add ConsumableBuilder and UNDEFINED type entry

--- src/main/kotlin/com/github/scroogemcfawk/csinvest/domain/Consumable.kt
+++ src/main/kotlin/com/github/scroogemcfawk/csinvest/domain/Consumable.kt
@@ -5,7 +5,47 @@ class Consumable(
     name: String,
     rarity: Rarity,
     val type: ConsumableType
-) : Item(name, rarity)
+) : Item(name, rarity) {
+
+    override fun toString(): String {
+        return "Consumable(name=$name, rarity=$rarity, type=$type)"
+    }
+
+}
+
+class ConsumableBuilder: ItemBuilder() {
+    var type = ConsumableType.UNDEFINED
+
+    override fun withName(name: String): ConsumableBuilder {
+        val new = this.copy()
+        new.name = name
+        return new
+    }
+
+    override fun withRarity(rarity: Rarity): ConsumableBuilder {
+        val new = this.copy()
+        new.rarity = rarity
+        return new
+    }
+
+    fun withType(type: ConsumableType): ConsumableBuilder {
+        val new = this.copy()
+        new.type = type
+        return new
+    }
+
+    override fun build(): Consumable {
+        return Consumable(name, rarity, type)
+    }
+
+    override fun copy(): ConsumableBuilder {
+        val copy = ConsumableBuilder()
+        copy.name = name
+        copy.rarity = rarity
+        copy.type = type
+        return copy
+    }
+}
 
 
 enum class ConsumableType {
@@ -13,5 +53,7 @@ enum class ConsumableType {
     PATCH,
     KEY,
     PASS,
-    GRAFFITI
+    GRAFFITI,
+
+    UNDEFINED;
 }
