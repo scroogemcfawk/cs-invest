4e0eb25b8cd28cd33942276a2b12fd73e4f56a50
scroogemcfawk <scroogemcfawk@gmail.com>
2024-01-15 21:28:40 +0700

    fix invalid tile selector and add debug logger to SectionScraper

--- src/main/kotlin/com/github/scroogemcfawk/csinvest/collector/csgostash/SectionScraper.kt
+++ src/main/kotlin/com/github/scroogemcfawk/csinvest/collector/csgostash/SectionScraper.kt
@@ -13,6 +13,10 @@ class SectionScraper(private val url: String) {
 
     private val log = LoggerFactory.getLogger(SectionScraper::class.java)
 
+    init {
+        log.atDebug()
+    }
+
     private val initial: Document by lazy {
 //        Jsoup.connect(url)
 //            .header("Accept-Language", "en-US")
@@ -30,15 +34,19 @@ class SectionScraper(private val url: String) {
     }
 
     fun get(): Elements {
+        log.debug("Scraping page '{}'", url)
+
         val result = Elements()
 
         // first page
         result.addAll(getItemsFromPage(initial))
+        log.debug("[1/{}] done", size)
 
         // remaining pages
         for (pageNumber in 2..size) {
             val page = Jsoup.connect("$url?page=$pageNumber").get()
             result.addAll(getItemsFromPage(page))
+            log.debug("[{}/{}] done", pageNumber, size)
         }
 
         return result
@@ -46,7 +54,11 @@ class SectionScraper(private val url: String) {
 
     private fun getItemsFromPage(page: Document): Elements {
         // select all boxes, excluding advert
-        return page.select("div.col-lg-4.col-md-6.col-widen.text-center:not(.adv-result-box-containers) > div:has(h4)")
+        val res = page.select("div.col-lg-4.col-md-6.col-widen.text-center:not(.adv-result-box-containers) > div:has(h3, h4)")
+        if (res.size < 1) {
+            log.warn("Unexpected empty tile list during page scraping.")
+        }
+        return res
     }
 
 }
