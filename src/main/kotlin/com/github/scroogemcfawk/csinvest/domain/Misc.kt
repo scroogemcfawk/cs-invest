c961d0dd7877106450b87dc71184ac76ba27eef7
scroogemcfawk <scroogemcfawk@gmail.com>
2024-01-16 19:58:39 +0700

    add toString, builder implementation, type UNDEFINED entry

--- src/main/kotlin/com/github/scroogemcfawk/csinvest/domain/Misc.kt
+++ src/main/kotlin/com/github/scroogemcfawk/csinvest/domain/Misc.kt
@@ -7,40 +7,59 @@ class Misc(
     val type: MiscType,
 ) : Item(name, rarity) {
 
+    override fun toString(): String {
+        return "Misc(name=$name, rarity=$rarity, type=$type)"
+    }
+
     override val builder: MiscBuilder
         get() = MiscBuilder().withName(name).withRarity(rarity).withType(type)
 
 }
 
 
-class MiscBuilder : ItemBuilder() {
+class MiscBuilder: ItemBuilder() {
+
+    var type = MiscType.UNDEFINED
 
     override fun withName(name: String): MiscBuilder {
-        TODO("Not yet implemented")
+        val new = this.copy()
+        new.name = name
+        return new
     }
 
     override fun withRarity(rarity: Rarity): MiscBuilder {
-        TODO("Not yet implemented")
+        val new = this.copy()
+        new.rarity = rarity
+        return new
     }
 
     fun withType(type: MiscType): MiscBuilder {
-        TODO("Not yet implemented")
+        val new = this.copy()
+        new.type = type
+        return new
     }
 
     override fun build(): Misc {
-        TODO("Not yet implemented")
+        return Misc(name, rarity, type)
     }
 
     override fun copy(): MiscBuilder {
-        TODO("Not yet implemented")
+        val copy = MiscBuilder()
+        copy.name = name
+        copy.rarity = rarity
+        copy.type = type
+        return copy
     }
+
 }
 
 
 enum class MiscType {
     AGENT,
     MUSIC_KIT,
-    COLLECTIBLE,
+    COLLECTIBLE, // PIN
     TAG,
-    TOOL
+    TOOL,
+
+    UNDEFINED;
 }
